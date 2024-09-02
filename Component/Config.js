1ecee1dcd17f135fcebc218b36156e90085647fe
Harshkesharwani2 <harsh@divyaltech.com>
2024-09-02 22:04:57 +0530

    Upgrade

--- Component/Config.js
+++ Component/Config.js
@@ -1,6 +1,6 @@
 //App Url
-export const url = 'http://10.0.2.2:5000';
-// export const url = 'http://Erp-school-env.eba-q984mcqv.us-east-1.elasticbeanstalk.com';
+// export const url = 'http://10.0.2.2:5000';
+export const url = 'http://Erp-school-env.eba-q984mcqv.us-east-1.elasticbeanstalk.com';
 // export const url = 'http://52.44.134.184';
 // export const url = 'http://192.168.29.96:5000';
 
